629749e5646b48940999ae5611fde61f5a6057d0
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-06-29 22:16:56 -0300

    Added the route to import categories

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCoategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCoategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(
   importCoategoryUseCase
 );
