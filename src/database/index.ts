93fc29e846a07d471e745472e5edba1f77b4332b
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-07-13 16:13:35 -0300

    typeorm updated to the latest version and refactored the configuration

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,21 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
-interface IOptions {
-  host: string;
-}
-
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "database_rentcars";
-  createConnection({
-    ...options,
-  });
+/* docker-compose exec app node --require ts-node/register ./node_modules/typeorm/cli.js migration:run -d src/database
+yarn typeorm migration:create src/database/migrations/CreateCategories */
+export const appDataSource = new DataSource({
+  type: "postgres",
+  host: "database_rentcars",
+  port: 5432,
+  username: "docker",
+  password: "rentcars123",
+  database: "rentcarsdb",
+  migrations: ["./src/database/migrations/*.ts"],
 });
+
+appDataSource
+  .initialize()
+  .then(async () => {
+    console.log("database initialize!");
+  })
+  .catch((err) => console.log(err));
