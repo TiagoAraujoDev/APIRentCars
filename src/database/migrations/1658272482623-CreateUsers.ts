4e5494cecb2e0f73197a857d2cc4a13313f4edea
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-07-23 21:15:27 -0300

    Add upload to add user's avatar

--- src/database/migrations/1658272482623-CreateUsers.ts
+++ src/database/migrations/1658272482623-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1658272482623 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
