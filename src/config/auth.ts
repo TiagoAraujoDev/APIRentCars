dc7f2fc355474229648929fbbf218e8ab0006531
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-09-05 09:24:10 -0300

    Add: Refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "cf9d571f3d9629589e8ddfcab1ac0b00",
+  expires_in_token: "15m",
+  secret_refresh_token: "f9a52ee501c85af61a466dc362206686",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,33 @@
+import { Repository } from "typeorm";
+
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { appDataSource } from "@shared/infra/typeorm";
+
+import { UserTokens } from "../entities/UserTokens";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = appDataSource.getRepository("users_tokens");
+  }
+
+  async create({
+    user_id,
+    refresh_token,
+    expires_date,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create({
+      user_id,
+      refresh_token,
+      expires_date,
+    });
+
+    await this.repository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export { UsersTokensRepository };
