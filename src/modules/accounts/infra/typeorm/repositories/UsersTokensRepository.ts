6e495326dd755a473c87af222e4fbd03a9c0e681
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-09-08 10:44:52 -0300

    Add: Method findByRefreshToken

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -44,6 +44,14 @@ class UsersTokensRepository implements IUsersTokensRepository {
   async deleteById(id: string): Promise<void> {
     await this.repository.delete(id);
   }
+
+  async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+    const userToken = this.repository.findOne({
+      where: { refresh_token },
+    });
+
+    return userToken;
+  }
 }
 
 export { UsersTokensRepository };
