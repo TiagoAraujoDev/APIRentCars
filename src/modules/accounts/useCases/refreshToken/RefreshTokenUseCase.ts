497a9c065e89fd4d6515d152bcfc6aae22fc9f81
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-09-26 10:38:42 -0300

    Fix: Change domain rule for refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -6,6 +6,11 @@ import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTok
 import { IDateProvider } from "@shared/container/providers/DateProviders/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
+interface ITokenResponse {
+  refresh_token: string;
+  token: string;
+}
+
 interface IPayload {
   sub: string;
   email: string;
@@ -20,7 +25,7 @@ class RefreshTokenUseCase {
     private dateProvider: IDateProvider
   ) {}
 
-  async execute(token: string): Promise<string> {
+  async execute(token: string): Promise<ITokenResponse> {
     const { email, sub: user_id } = verify(
       token,
       auth.secret_refresh_token
@@ -53,7 +58,15 @@ class RefreshTokenUseCase {
       user_id,
     });
 
-    return refresh_token;
+    const newToken = sign({}, auth.secret_token, {
+      subject: user_id,
+      expiresIn: auth.expires_in_token,
+    });
+
+    return {
+      refresh_token,
+      token: newToken,
+    };
   }
 }
 
