f5ff1f0da6beec2e4393b560d99b19e6e7dd08dc
Tiago Muniz de Araujo <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-09-26 22:52:21 -0300

    feat: variable to set local and S3 storage

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,11 +1,15 @@
+import * as dotenv from "dotenv";
 import { container } from "tsyringe";
 
 import { IDateProvider } from "./DateProviders/IDateProvider";
 import { DayjsDateProvider } from "./DateProviders/implamentations/DayjsDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
+
+dotenv.config();
 
 container.registerSingleton<IDateProvider>(
   "DayjsDateProvider",
@@ -17,7 +21,12 @@ container.registerInstance<IMailProvider>(
   new EtherealMailProvider()
 );
 
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  S3StorageProvider
+  diskStorage[process.env.disk]
 );
